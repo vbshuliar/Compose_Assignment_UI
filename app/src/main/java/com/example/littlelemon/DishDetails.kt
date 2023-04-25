ccf5937c8e87831d10625a187529e70adf8b20cc
vbshuliar <vbshuliar@gmail.com>
2023-04-25 23:16:28 +0100

    fix: button fixed

--- app/src/main/java/com/example/littlelemon/DishDetails.kt
+++ app/src/main/java/com/example/littlelemon/DishDetails.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
@@ -24,6 +25,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.littlelemon.ui.theme.LittleLemonColor
 
 @Composable
 fun DishDetails(id: Int) {
@@ -48,13 +50,18 @@ fun DishDetails(id: Int) {
                 style = MaterialTheme.typography.body1
             )
             Counter()
-            Button(onClick = { /*TODO*/ }) {
+            Button(
+                onClick = { /*TODO*/ },
+                colors = ButtonDefaults.buttonColors(backgroundColor = LittleLemonColor.yellow)
+            ) {
                 Text(
                     text = stringResource(id = R.string.add_for) + " " + "$${dish.price}",
                     textAlign = TextAlign.Center,
                     modifier = Modifier
                         .fillMaxWidth()
-                        .align(CenterVertically)
+                        .align(CenterVertically),
+                    style = MaterialTheme.typography.button,
+                    color = LittleLemonColor.green
                 )
             }
         }
